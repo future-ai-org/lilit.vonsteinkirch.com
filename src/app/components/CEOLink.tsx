821bc95cddf6609312713d328f17bae246a0819b
Marina von Steinkirch, PhD <marina@vonsteinkirch.com>
2025-08-30 20:21:32 -0300

    Update CEOLink.tsx

--- src/app/components/CEOLink.tsx
+++ src/app/components/CEOLink.tsx
@@ -4,7 +4,7 @@ export default function CEOLink() {
   return (
     <div className="ceo-link group">
       <a 
-        href="https://www.bt3gl.xyz" 
+        href="https://www.vonsteinkirch.com" 
         target="_blank" 
         rel="noopener noreferrer" 
         className="ceo-link-text glow-link"
