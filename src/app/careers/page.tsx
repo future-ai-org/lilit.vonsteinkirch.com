39cbb1d494c7b06b89598bef21189fc934df0c3c
Marina von Steinkirch, PhD <marina@vonsteinkirch.com>
2025-08-30 20:22:41 -0300

    Update page.tsx

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -29,33 +29,6 @@ export default function JobsPage() {
               ). this is an ongoing opportunity.
             </p> 
           </div>
-
-          <div className="careers-apply-section">
-            <h2 className="values-title title-glow careers-subtitle">
-              <span className="values-title-text">how to apply</span>
-              <div className="values-title-glow title-bg" />
-            </h2>
-            <div className="values-card-text">
-              <p>
-                to apply for either position, please send an introduction letter (in plain text) to{' '}
-                <span className="careers-highlight-wrapper">
-                  <a href="mailto:hello@lilit.ai" className="careers-highlight-link">
-                    hello@lilit.ai
-                  </a>
-                  <div className="values-title-glow title-bg overlay-fill" />
-                </span> with the following:
-              </p>
-              <ul className="careers-list">
-                <li>your relevant experience and expertise</li>
-                <li>links to your online presence (personal website, youtube, github, etc.)</li>
-                <li>why you&apos;re interested in joining our mission and which of our values resonate most with you</li>
-                <li>your birth chart or big 3, and anything you think is interesting about your avatar</li>
-              </ul>
-              <p className="careers-compensation-text">
-                we look forward to connecting with you and hopefully welcoming you to our team!
-              </p>
-            </div>
-          </div>
         </div>
       </div>
     </div>
